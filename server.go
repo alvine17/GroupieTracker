98e09bb3bd46df7c9b61fe9e19c5ff5f799933eb
MALVINE <mpanzu.alvine@ynov.com>
2023-02-05 16:07:52 +0100

    recuperation api site web

--- server.go
+++ server.go
@@ -1,38 +1,53 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
+	"text/template"
 )
 
-func main() {
-	callAPI()
-
-	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	// 	fmt.Fprintf(w, global)
-	// })
-
-	// fs := http.FileServer(http.Dir("static/"))
-	// http.Handle("/static/", http.StripPrefix("/static/", fs))
-
-	// http.ListenAndServe(":8080", nil)
+type Artist struct {
+	Id      int
+	Image   string
+	Name    string
+	Members []string
 }
 
-func callAPI() {
+func main() {
+
+	tmpl := template.Must(template.ParseFiles("index.html"))
+
+	global := callAPI()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl.Execute(w, global)
+	})
+
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func callAPI() Artist {
 	response, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
+	var artist Artist
 
-	if err != nil {
-		fmt.Print(err.Error())
-		os.Exit(1)
-	}
-
-	responseData, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(string(responseData))
 
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	json.Unmarshal(body, &artist.Members)
+
+	return artist
 }
