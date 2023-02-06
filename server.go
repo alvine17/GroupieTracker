6f84a805e05bb71d493259e7aed269ccbb6e6e6c
renvel <envel.righini@ynov.com>
2023-02-06 15:38:08 +0100

    feat add  api informations on the website

--- server.go
+++ server.go
@@ -2,24 +2,30 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"text/template"
 )
 
-type Artist struct {
-	Id      int
-	Image   string
-	Name    string
-	Members []string
+type Artists []struct {
+	ID      int      `json:"id"`
+	Image   string   `json:"image"`
+	Name    string   `json:"name"`
+	Members []string `json:"members"`
 }
 
 func main() {
 
 	tmpl := template.Must(template.ParseFiles("index.html"))
 
+	// global := callAPI()
 	global := callAPI()
+
+	for _, i := range global {
+		fmt.Println(i.Name)
+	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		tmpl.Execute(w, global)
@@ -32,9 +38,8 @@ func main() {
 
 }
 
-func callAPI() Artist {
+func callAPI() Artists {
 	response, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
-	var artist Artist
 
 	if err != nil {
 		log.Fatal(err)
@@ -43,11 +48,13 @@ func callAPI() Artist {
 	defer response.Body.Close()
 
 	body, err := ioutil.ReadAll(response.Body)
-
+	// fmt.Println(string(body))
 	if err != nil {
 		log.Fatal(err)
 	}
-	json.Unmarshal(body, &artist.Members)
+	array := Artists{}
+	json.Unmarshal(body, &array)
 
-	return artist
+	return array
+
 }
