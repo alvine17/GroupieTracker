090c6a527cf546230dd526cd6fe941084541578f
ronyk10 <rony.klock@ynov.com>
2023-02-07 16:41:41 +0100

    chore organization

---
+++ server.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"text/template"
+)
+
+type Artist struct {
+	Membre string
+}
+type Artists []struct {
+	Id      int
+	Image   string
+	Name    string
+	Members []string
+}
+
+func main() {
+
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	global := callAPI()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl := template.Must(template.ParseFiles("./static/index.html"))
+		tmpl.Execute(w, global)
+	})
+
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func callAPI() Artists {
+	response, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
+	var artist Artists
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	json.Unmarshal(body, &artist)
+
+	return artist
+}
